d8eaa07b5cf389c449afc803aec1da9ee4429531
toya <toya24xxx@i.softbank.jp>
2022-05-15 16:32:07 +0900

    UIの微調整

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,9 @@
 import type { GetServerSideProps, NextPage } from 'next';
-
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import nookies from 'nookies';
-
-import { firebaseAdmin } from '../firebaseAdmin'; // この後に実装するファイル
-import { logout } from '../utils'; // 上記で実装したファイル
+import { firebaseAdmin } from '../firebaseAdmin';
+import { logout } from '../utils';
 
 const DashboardPage: NextPage<{ email: string }> = ({ email }) => {
   const router = useRouter();
@@ -15,15 +14,22 @@ const DashboardPage: NextPage<{ email: string }> = ({ email }) => {
   };
 
   return (
-    <div className='vh-100 d-flex justify-content-center align-items-center'>
-      <div className='card'>
-        <h1 className='card-header fs-6'>Dashboard Pages</h1>
-        <div className='card-body'>
-          <h2 className='fs-6'>email: {email}</h2>
-          <div className='mt-3 text-center'>
-            <button className='btn btn-primary' onClick={onLogout}>
-              Logout
-            </button>
+    <div className='h-100 p-5'>
+      <Head>
+        <title>Dashboard Page</title>
+        <meta name='description' content='ダッシュボード画面です。' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='h-100 d-flex justify-content-center align-items-center'>
+        <div className='card'>
+          <h1 className='card-header fs-6'>Dashboard Pages</h1>
+          <div className='card-body'>
+            <h2 className='fs-6'>email: {email}</h2>
+            <div className='mt-3 text-center'>
+              <button className='btn btn-primary' onClick={onLogout}>
+                Logout
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import type { FormEvent } from 'react';
 
@@ -18,39 +19,46 @@ const LoginPage: NextPage = () => {
   };
 
   return (
-    <div className='vh-100 d-flex justify-content-center align-items-center'>
-      <div className='card'>
-        <h1 className='card-header fs-6'> Login</h1>
+    <div className='h-100 p-5'>
+      <Head>
+        <title>Login Page</title>
+        <meta name='description' content='ログイン画面です。' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='h-100 d-flex justify-content-center align-items-center'>
+        <div className='card'>
+          <h1 className='card-header fs-6'> Login</h1>
 
-        <div className='card-body'>
-          <form onSubmit={onSubmit}>
-            <div>
-              <label htmlFor='email'>Email</label>
-              <input
-                className='form-control'
-                id='email'
-                value={email}
-                onInput={(e) => setEmail(e.currentTarget.value)}
-              />
-            </div>
+          <div className='card-body'>
+            <form onSubmit={onSubmit}>
+              <div>
+                <label htmlFor='email'>Email</label>
+                <input
+                  className='form-control'
+                  id='email'
+                  value={email}
+                  onInput={(e) => setEmail(e.currentTarget.value)}
+                />
+              </div>
 
-            <div>
-              <label htmlFor='password'>Password</label>
+              <div>
+                <label htmlFor='password'>Password</label>
 
-              <input
-                id='password'
-                type='password'
-                className='form-control'
-                value={password}
-                onInput={(e) => setPassword(e.currentTarget.value)}
-              />
-            </div>
-            <div className='mt-3 text-center'>
-              <button className='btn btn-primary' type='submit'>
-                Login
-              </button>
-            </div>
-          </form>
+                <input
+                  id='password'
+                  type='password'
+                  className='form-control'
+                  value={password}
+                  onInput={(e) => setPassword(e.currentTarget.value)}
+                />
+              </div>
+              <div className='mt-3 text-center'>
+                <button className='btn btn-primary' type='submit'>
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
